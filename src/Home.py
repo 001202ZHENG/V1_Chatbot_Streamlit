8e61ee7b136608c1d45b6df382675e4a5fd2b339
zwzwz <113729384+001202ZHENG@users.noreply.github.com>
2024-04-16 22:42:53 +0200

    update

--- src/Home.py
+++ src/Home.py
@@ -2,27 +2,23 @@ import streamlit as st
 
 
 #Config
-st.set_page_config(layout="wide", page_icon="💬", page_title="Robby | Chat-Bot 🤖")
+st.set_page_config(layout="wide", page_icon="💬", page_title="AI Chatbot 🤖 Voice Of Customer")
 
 
 #Contact
 with st.sidebar.expander("📬 Contact"):
 
     st.write("**GitHub:**",
-"[yvann-hub/Robby-chatbot](https://github.com/yvann-hub/Robby-chatbot)")
-
-    st.write("**Medium:** "
-"[@yvann-hub](https://medium.com/@yvann-hub)")
-
-    st.write("**Twitter:** [@yvann_hub](https://twitter.com/yvann_hub)")
-    st.write("**Mail** : barbot.yvann@gmail.com")
-    st.write("**Created by Yvann**")
+"[V1_Chatbot_Streamlit](https://github.com/001202ZHENG/V1_Chatbot_Streamlit)")
+    st.write("**Mail** :", "zheng.wan@student-cs.fr [Deloitte Team]")
+    st.write("**Mail** :", "TNuss@deloitte.fr [Deloitte Team]")
+    st.write("** By “Voice of Customers” Team**")
 
 
 #Title
 st.markdown(
     """
-    <h2 style='text-align: center;'>Robby, your data-aware assistant 🤖</h1>
+    <h2 style='text-align: center;'>VoC, where your voice is heard and advice is served. 🤖</h1>
     """,
     unsafe_allow_html=True,)
 
@@ -32,16 +28,13 @@ st.markdown("---")
 #Description
 st.markdown(
     """ 
-    <h5 style='text-align:center;'>I'm Robby, an intelligent chatbot created by combining 
-    the strengths of Langchain and Streamlit. I use large language models to provide
-    context-sensitive interactions. My goal is to help you better understand your data.
-    I support PDF, TXT, CSV, Youtube transcript 🧠</h5>
+    <h5 style='text-align:center;'>Hey there! I'm VoC, your friendly chatbot here to lend an ear to your HR service needs and dish out some heartfelt advice. 🤖 I'm all about using cutting-edge language models to give you spot-on insights and make navigating your HR services a breeze. Let's work together to iron out any wrinkles in your HR processes! 💼 I'm your go-to guy for handling PDFs, TXTs, and CSV files. Let's chat! 🧠</h5>
     """,
     unsafe_allow_html=True)
 st.markdown("---")
 
 
-#Robby's Pages
+'''#Robby's Pages
 st.subheader("🚀 Robby's Pages")
 st.write("""
 - **Robby-Chat**: General Chat on data (PDF, TXT,CSV) with a [vectorstore](https://github.com/facebookresearch/faiss) (index useful parts(max 4) for respond to the user) | works with [ConversationalRetrievalChain](https://python.langchain.com/en/latest/modules/chains/index_examples/chat_vector_db.html)
@@ -49,12 +42,12 @@ st.write("""
 - **Robby-Youtube**: Summarize YouTube videos with [summarize-chain](https://python.langchain.com/en/latest/modules/chains/index_examples/summarize.html)
 """)
 st.markdown("---")
-
+'''
 
 #Contributing
-st.markdown("### 🎯 Contributing")
+st.markdown("### 🎯 Contribution")
 st.markdown("""
-**Robby is under regular development. Feel free to contribute and help me make it even more data-aware!**
+**This chatbot is developed through the collaborative efforts of the Voice of Customers teams from ESSEC Business School & CentraleSupélec, consisting of Data Sciences & Business Analytics students, along with the Deloitte Team. 🚀**
 """, unsafe_allow_html=True)
 
 
