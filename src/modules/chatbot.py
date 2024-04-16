1eb664b7c7b229f51752397efcb7d43987dbca14
zwzwz <113729384+001202ZHENG@users.noreply.github.com>
2024-04-16 23:06:41 +0200

    Update chatbot.py

--- src/modules/chatbot.py
+++ src/modules/chatbot.py
@@ -17,7 +17,8 @@ class Chatbot:
 
     qa_template = """
         You are a helpful AI assistant named VoC. The user gives you a file its content is represented by the following pieces of context, use them to answer the question at the end.
-        If the question is not related to the context, or no file provided,just politely respond according to your knowledge.
+        If you don't know the answer, just say you don't know. Do NOT try to make up an answer.
+        If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
         Use as much detail as possible when responding.
 
         context: {context}
