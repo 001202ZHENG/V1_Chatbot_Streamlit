73f3b67affdc9250ffff118bcc6e53f1a3904912
larrysangfake <larry.sang@student-cs.fr>
2024-05-26 10:56:41 +0200

    fix q48 pct

--- src/Home.py
+++ src/Home.py
@@ -1366,7 +1366,7 @@ if dashboard == 'Section 6: Payroll':
 
     #Payroll team
     q48_compensation_count = (filtered_data.iloc[:,48] == 'Yes').sum()
-    q48_compensation_pct = q36_compensation_count/len(filtered_data) * 100
+    q48_compensation_pct = q48_compensation_count/len(filtered_data) * 100
 
     st.write("Payroll Team")
     
